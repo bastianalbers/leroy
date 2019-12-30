ffb6f6763897b313a5a849be63ad38fd617c4231
Bastian Albers <info@bastianalbers.de>
2019-12-30 16:33:49 +0100

    lint

--- src/app/gameConfig.js
+++ src/app/gameConfig.js
@@ -1,8 +1,8 @@
-import Phaser from 'phaser';
-import bootScene from './bootScene';
-import playScene from './playScene';
-import menuScene from './menuScene';
-import endScene from './endScene';
+import Phaser from 'phaser'
+import bootScene from './bootScene'
+import playScene from './playScene'
+import menuScene from './menuScene'
+import endScene from './endScene'
 
 export default {
   type: Phaser.AUTO,
@@ -13,4 +13,4 @@ export default {
   url: 'https://github.com/samme/phaser-parcel',
   banner: { text: 'white', background: ['#FD7400', '#FFE11A', '#BEDB39', '#1F8A70', '#004358'] },
   scene: [bootScene, menuScene, playScene, endScene]
-};
+}
